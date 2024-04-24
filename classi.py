06d7645221201d8f9f5adfe653b60cc3023e922b
Vikyfy <136378367+Vikyfy@users.noreply.github.com>
2024-04-24 16:11:32 +0300

    Create classi.py

---
+++ classi.py
@@ -0,0 +1,33 @@
+# class Rectangle:
+#     def __init__(self, height, weight):
+#         self.height = height
+#         self.weight = weight
+#     def p(self):
+#         return (self.weight + self.height)*2
+#     def s(self):
+#         return (self.weight * self.height)
+#
+#
+# r = Rectangle(20, 30)
+#
+#
+# print(r.height, r.weight)
+# print(r.s())
+
+# class triangle:
+#     def __init__(self, s1, s2,  s3):
+#         self.s1 = s1
+#         self.s2 = s2
+#         self.s3 = s3
+#     def p(self):
+#         return (self.s1 + self.s2 + self.s3)
+#     def s(self):
+#         return (self.s1 * self.s2 )/2
+#
+# tri = triangle(20, 20, 40)
+# print (tri.s())
+
+def k_press(event):
+    print(event.keysym)
+
+def main():
